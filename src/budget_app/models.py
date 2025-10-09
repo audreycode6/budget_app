91d5100a7908f2ef29991f4a1b2f852affcf5510
Audrey Theriault-Allaire <audrey2699@gmail.com>
2025-10-09 10:21:40 -0700

    set up auth: signup, login, logout routes and add pw encryption to models.py

--- src/budget_app/models.py
+++ src/budget_app/models.py
@@ -1,10 +1,17 @@
-from app import db # import the SQLAlchemy instance
+from .app import db # import the SQLAlchemy instance
+from werkzeug.security import generate_password_hash, check_password_hash
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
     password_hash = db.Column(db.String(200), nullable=False)
     budgets = db.relationship('Budget', backref='user', cascade='all, delete')
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
 
 class Budget(db.Model):
     id = db.Column(db.Integer, primary_key=True)
---
+++ src/budget_app/utils.py
@@ -0,0 +1,1 @@
+# TODO add helper functions for app.py routes
